fa8827be9843f0ee46d045db15fba65e5f0e82b7
Michael AHS <michael.ahs@soprasteria.com>
2020-11-03 15:12:41 +0100

    Added code for C# HelloWorld

---
+++ CSharp/CSharp1HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp1HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World of C# 1.0- (2002-)!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/CSharp9HelloWorld/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+
+Console.WriteLine("Hello World of C# 9.0- (2020-)!");
